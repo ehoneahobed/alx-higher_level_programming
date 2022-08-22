8707bffb8d11a8ad4b6fa7c9ed04c04184f575df
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-08-22 20:17:32 +0000

    Update 8-concat_edges

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[106:112] + str[:7]
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
