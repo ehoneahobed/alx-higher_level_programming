e1f62ad5d8cf966b8f5119fe797b5c0fbe860566
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-08-25 19:04:22 +0000

    Update solution for pycodestyle

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0 as add_func
-    
+    from add_0 import add
+
     a = 1
     b = 2
-
-    sum = add_func.add(a, b)
-    print(f"{a} + {b} = {sum}")
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
