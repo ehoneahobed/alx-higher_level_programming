03b956f8dfe56152a0f857f8b007218b5719e455
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-08-29 18:49:32 +0000

    Add solution to task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+        
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+    new_tuple = ((tuple_a[0] + tuple_b[0]),  (tuple_a[1] + tuple_b[1]))
+    return (new_tuple)
