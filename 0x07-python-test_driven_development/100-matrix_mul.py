6818969c1f97cf8ed8d5c28ab50f6df31db06ea2
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-09-21 09:43:50 +0000

    Update solution to task 6

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -25,48 +25,59 @@ def matrix_mul(m_a, m_b):
         A new list which is the outcome of the multiplication
 
     """
-    
+
     if type(m_a) is not list:
         raise TypeError("m_a must be a list")
     if type(m_b) is not list:
         raise TypeError("m_b must be a list")
+
     for x in m_a:
         if type(x) is not list:
             raise TypeError("m_a must be a list of lists")
     for x in m_b:
         if type(x) is not list:
             raise TypeError("m_b must be a list of lists")
-    if len(m_a) < 1:
+
+    if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
-    if len(m_b) < 1:
+    if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
-    arowlen = len(m_a[0])
-    if arowlen < 1:
-        raise ValueError("m_a can't be empty")
-    browlen = len(m_b[0])
-    if browlen < 1:
-        raise ValueError("m_b can't be empty")
+
     for row in m_a:
-        if len(row) != arowlen:
-            raise TypeError("each row of m_a must should be of the same size")
-        for col in row:
-            if type(col) is not float and type(col) is not int:
+        for x in row:
+            if type(x) is not int and type(x) is not float:
                 raise TypeError("m_a should contain only integers or floats")
     for row in m_b:
-        if len(row) != browlen:
-            raise TypeError("each row of m_b must should be of the same size")
-        for col in row:
-            if type(col) is not float and type(col) is not int:
+        for x in row:
+            if type(x) is not int and type(x) is not float:
                 raise TypeError("m_b should contain only integers or floats")
-    if arowlen != len(m_b):
+
+    row_len = []
+    for row in m_a:
+        row_len.append(len(row))
+    if not all(elem == row_len[0] for elem in row_len):
+            raise TypeError("each row of m_a must should be of the same size")
+    row_len = []
+    for row in m_b:
+        row_len.append(len(row))
+    if not all(elem == row_len[0] for elem in row_len):
+            raise TypeError("each row of m_b must should be of the same size")
+
+    a_col = 0
+    for col in m_a[0]:
+        a_col += 1
+    b_row = 0
+    for row in m_b:
+        b_row += 1
+
+    if a_col != b_row:
         raise ValueError("m_a and m_b can't be multiplied")
-    newmatrix = []
-    for x in range(len(m_a)):
-        newrow = []
-        for y in range(browlen):
-            sum = 0
-            for z in range(arowlen):
-                sum += m_a[x][z] * m_b[z][y]
-            newrow.append(sum)
-        newmatrix.append(newrow)
-    return newmatrix
+
+    result = [[0 for x in range(len(m_b[0]))] for y in range(len(m_a))]
+
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                result[i][j] += m_a[i][k] * m_b[k][j]
+
+    return result
