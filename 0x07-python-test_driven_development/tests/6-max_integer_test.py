ca009908606b0c9c29f2d2473d0067e89c49242b
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-09-21 09:30:07 +0000

    Make 6-max test executable

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -45,12 +45,12 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_string(self):
         """Test a string."""
-        string = "Brennan"
+        string = "Ehoneah"
         self.assertEqual(max_integer(string), 'r')
 
     def test_list_of_strings(self):
         """Test a list of strings."""
-        strings = ["Brennan", "is", "my", "name"]
+        strings = ["Ehoneah", "is", "my", "name"]
         self.assertEqual(max_integer(strings), "name")
 
     def test_empty_string(self):
