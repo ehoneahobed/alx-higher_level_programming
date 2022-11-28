c97c68bbf82cc46e0e4b04d76059b07ccfd9aeea
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-11-28 18:11:57 +0000

    Update indentation spaces

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const argc = process.argv.length;
 
 if (argc > 2) {
-    console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 } else {
-    console.log('No argument');
+  console.log('No argument');
 }
