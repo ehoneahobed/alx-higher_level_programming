e82fd348f11fad39d16f5ee9dc49083b631b974e
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-11-28 17:14:52 +0000

    Change double quotes to single

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const myVar = "JavaScript is amazing";
 
-console.log(myVar)
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const myVar = "C is fun\nPython is cool\nJavaScript is amazing";
 
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
 console.log(myVar);
