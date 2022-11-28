0ee142a087adbe67909154e0300ac44f2c060772
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-11-28 19:34:24 +0000

    Add solution to task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const x = process.argv[2];
+
+if (!parseInt(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
