20623be0f54a7a01f87abde8cdb76330168d6ffc
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-11-29 19:59:13 +0000

    Add solution to task 8

--- 0x13-javascript_objects_scopes_closures/test.js
+++ 0x13-javascript_objects_scopes_closures/test.js
@@ -4,3 +4,8 @@
 // console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
 // console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
 // console.log(nbOccurences(["S", 12, "c", "S", "School", 8], "S"));
+
+// const esrever = require('./8-esrever').esrever;
+
+// console.log(esrever([1, 2, 3, 4, 5]));
+// console.log(esrever(['School', 89, { id: 12 }, 'String']));
