0cadf5e7184d6b708e54ff27112f4ccbe061ccc2
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-11-29 19:01:31 +0000

    Add solution to task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+/**
+ * Square class that inherits from previous square class
+ */
+const PrevSquare = require('./5-square');
+
+class Square extends PrevSquare {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    const myChar = (c === undefined ? 'X' : c);
+    for (let i = 0; i < this.size; i++) {
+      let myVar = '';
+      let y = 0;
+      while (y < this.size) {
+        myVar += myChar;
+        y++;
+      }
+
+      console.log(myVar);
+    }
+  }
+}
+
+module.exports = Square;
+
+const s1 = new Square(4);
+s1.charPrint();
+
+s1.charPrint('C');
