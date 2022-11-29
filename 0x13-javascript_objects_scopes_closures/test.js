98f53bbc629c1d7a1b1c05ffc331cceada6a7a2a
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-11-29 19:34:47 +0000

    Add solution to task 7

---
+++ 0x13-javascript_objects_scopes_closures/test.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// const nbOccurences = require('./7-occurrences').nbOccurences;
+
+// console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
+// console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
+// console.log(nbOccurences(["S", 12, "c", "S", "School", 8], "S"));
