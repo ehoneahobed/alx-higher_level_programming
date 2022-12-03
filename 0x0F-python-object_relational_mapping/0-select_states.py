fd328a83c3266e69b32eddd0bf61d34cc521d0d6
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-12-03 21:02:03 +0000

    Add documentation to 0-select_states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
+
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
     db_connect = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
