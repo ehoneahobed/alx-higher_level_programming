9dce54e3d51df2d92c76940be7fdb81d2c81065f
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-12-03 22:06:13 +0000

    Update solution to task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     db_cursor = db_connect.cursor()
     db_cursor.execute(
         "SELECT * FROM states WHERE name LIKE \
-                    BINARY %(name)s ORDER BY state.id ASC", {'name': argv[4]})
+                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
 
     rows_selected = db_cursor.fetchall()
 
