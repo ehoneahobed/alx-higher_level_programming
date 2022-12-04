851c3f53e191d5eae2caa644acb66f5c0c37a005
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-12-04 15:08:49 +0000

    Update solution to task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,7 +5,7 @@ from the database `hbtn_0e_6_usa`.
 """
 
 from sys import argv
-from model_state import Base, State
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -15,9 +15,10 @@ if __name__ == "__main__":
     from the database.
     """
 
-    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
-    engine = create_engine(db_uri)
+
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
 
     session = Session()
