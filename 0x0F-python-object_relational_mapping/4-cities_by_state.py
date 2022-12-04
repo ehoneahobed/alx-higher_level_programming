bd5c95b59258c58e03b47fff4190aba23016cd0d
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-12-04 10:44:07 +0000

    Update solution to task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-    Access to the database and get the cities
+    Access the database and get the cities
     from the database.
     """
 
@@ -17,7 +17,9 @@ if __name__ == '__main__':
                             user=argv[1], passwd=argv[2], db=argv[3])
 
     with db_connect.cursor() as db_cursor:
-        db_cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+        db_cursor.execute("SELECT cities.id, cities.name, states.name \
+                                FROM cities JOIN states ON cities.state_id \
+                                = states.id ORDER BY cities.id ASC")
         rows_selected = db_cursor.fetchall()
 
     if rows_selected is not None:
