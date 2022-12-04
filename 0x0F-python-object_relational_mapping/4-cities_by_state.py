af24b26e4a7ed5ef82781ccba446673688621701
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-12-04 10:02:39 +0000

    Add solution to task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""
+This script lists all cities from
+the database `hbtn_0e_4_usa`.
+"""
 
 import MySQLdb as db
 from sys import argv
 
 if __name__ == '__main__':
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+
     db_connect = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
 
