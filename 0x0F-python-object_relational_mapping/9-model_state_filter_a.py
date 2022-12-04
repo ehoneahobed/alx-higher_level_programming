09dfdb2e355b3782cdc1b8ec5cc3090fcca52499
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-12-04 15:22:04 +0000

    Update task 9 solution

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -24,6 +24,7 @@ if __name__ == "__main__":
 
     session = Session()
 
-    state = session.query(State).filter(State.name.contains('a'))
-    if state is not None:
-        print('{0}: {1}'.format(state.id, state.name))
+    states = session.query(State).filter(State.name.contains('a'))
+    if states is not None:
+        for state in states:
+            print('{0}: {1}'.format(state.id, state.name))
