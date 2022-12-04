3afbe9550987d41fea6ad14410cee9b3174f9050
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-12-04 12:01:55 +0000

    Add docstring to task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
+"""
+This script  takes in the name of a state
+as an argument and lists all cities of that
+state, using the database `hbtn_0e_4_usa`.
+"""
 
 import MySQLdb as db
 from sys import argv
 
 if __name__ == "__main__":
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+
     db_connect = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
 
